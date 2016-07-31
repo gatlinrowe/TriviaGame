8931dfc1c3c7b23d1457756555b6b4876e6f0858
gatlinrowe <gatlinrowe@gmail.com>
2016-07-31 04:00:23 -0400

    fixed multiple click with .unbind

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	var questions = new Array(4);
-	questions[0] = new Question ("What percentage of our brain do we actually use?", "10%", "90%", "100%", "it is unknown", 3, "Neurologists describe that we use virtually every part of the brain, and that (most of) the brain is active almost all the time.", "assets/images/brain.jpg");
-	questions[1] = new Question ("Which is false?", "Alcohol keeps you warm", "Tomatoes are a fruit", "Salty water boils slower", "the capital of Australia is Canberra", 1,"Alcohol may make your skin feel warm, but this apparent heatwave is deceptive. A nip or two actually causes your blood vessels to dilate, moving warm blood closer to the surface of your skin, making you feel warmer temporarily. At the same time, however, those same veins pumping blood closer to the skin's surface causing you to lose core body heat.","assets/images/alcohol.jpg" );
-	questions[2] = new Question ("What class did Albert Einstein fail as a kid?", "Mathematics", "Physics", "P.E", "None of the above", 4, "He failed one entry exam to a school but in fact, he actually excelled at mathematics throughout his schooling and even considered becoming a mathematician for a time","assets/images/einstein.jpg");
-	questions[3] = new Question ("what color is your blood in your veins, before it touches oxygen?", "Purple", "Red", "Black", "Blue ", 2, "While many believe that de-oxygenated blood is blue, it is actually a myth because human blood is always red, hemoglobins actually use iron to move oxygen, so you get red blood from oxygenated iron, rust.", "assets/images/blood.png");
+	questions[0] = new Question ("What percentage of our brain do we actually use?", "10%", "90%", "100%", "it is unknown", "3", "Neurologists describe that we use virtually every part of the brain, and that (most of) the brain is active almost all the time.", "assets/images/brain.jpg");
+	questions[1] = new Question ("Which is false?", "Alcohol keeps you warm", "Tomatoes are a fruit", "Salty water boils slower", "the capital of Australia is Canberra", "1","Alcohol may make your skin feel warm, but this apparent heatwave is deceptive. A nip or two actually causes your blood vessels to dilate, moving warm blood closer to the surface of your skin, making you feel warmer temporarily. At the same time, however, those same veins pumping blood closer to the skin's surface causing you to lose core body heat.","assets/images/alcohol.jpg" );
+	questions[2] = new Question ("What class did Albert Einstein fail as a kid?", "Mathematics", "Physics", "P.E", "None of the above", "4", "He failed one entry exam to a school but in fact, he actually excelled at mathematics throughout his schooling and even considered becoming a mathematician for a time","assets/images/einstein.jpg");
+	questions[3] = new Question ("what color is your blood in your veins, before it touches oxygen?", "Purple", "Red", "Black", "Blue ", "2", "While many believe that de-oxygenated blood is blue, it is actually a myth because human blood is always red, hemoglobins actually use iron to move oxygen, so you get red blood from oxygenated iron, rust.", "assets/images/blood.png");
 
 	var currentQuestion = -1;
 	var correct = 0;
@@ -25,17 +25,18 @@ $(document).ready(function() {
 		$("#question").html("<img src='"+questions[currentQuestion].img+"'>");
 		$("#1").html(questions[currentQuestion].description);
 		setTimeout(function() {
-			if (currentQuestion == questions.length){
+			if (currentQuestion+1 == questions.length){
 				scoreScreen();
 			}
 			else {
 			quiz();
 			}
-		}, 5000);
+		}, 2000);
 
 	}
 	function quiz(){
 		currentQuestion++;
+		console.log("question #" +currentQuestion);
 		correctAnswer = questions[currentQuestion].correctAnswer;
 		console.log("answer is #"+correctAnswer);
 		$(".content").empty();
@@ -45,14 +46,14 @@ $(document).ready(function() {
 		$("#3").html("<p>"+questions[currentQuestion].answer3+"</p>");
 		$("#4").html("<p>"+questions[currentQuestion].answer4+"</p>");
 		
-		$(".answer").click( function() {
+		$(".answer").unbind().click( function() {
 			console.log("answer selected:"+$(this).attr("id"));
-			if ($(this).attr("id") == correctAnswer) {
+			if ($(this).attr("id") === correctAnswer) {
 				//gotIt = "CORRECT";
 				correct++;
 				console.log("right answers:"+correct);
 				gotIt("CORRECT");
-			}else if ($(this).attr("id") != correctAnswer){
+			}else if ($(this).attr("id") !== correctAnswer){
 				//gotIt = "INCORRECT";
 				wrong++;
 				console.log("wrong answers:"+wrong);
@@ -79,7 +80,7 @@ $(document).ready(function() {
 
 	}
 
-	$("#intro").click( function() {
+	$("#intro").unbind().click( function() {
 		currentQuestion = -1;
 		correct = 0;
 		wrong = 0;
